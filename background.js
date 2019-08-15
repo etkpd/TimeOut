40b691c89fc33f9659578328866cc4b48cfd7a0b
etkpd <40926506+etkpd@users.noreply.github.com>
2019-08-15 16:50:14 -0400

    used browserAction.setBadgeText to display timer.

--- background.js
+++ background.js
@@ -1,20 +1,47 @@
-/* let array = ['aq', 'cd']
+chrome.browserAction.setBadgeText({ 'text': '00:00'});
+chrome.browserAction.setBadgeBackgroundColor({ 'color': "#777" });
 
-function init(){
-  localStorage.setItem("bannedSites", JSON.stringify(array));
-}
- */
-function Update(t, tabId, url){
-  //console.log(t, tabId, url)
-}
+let activeTabId = null;
+let timeStart = null;
 
 function HandleUpdate(tabId, changeInfo, tab){
-  Update(new Date(), tabId, changeInfo.url)
+  initialUpdate(new Date(), tabId)
 }
 
+function HandleUpdateActiveTab(activeInfo){
+  initialUpdate(new Date(), activeInfo.tabId)
+}
 
+function WindowHasChanged(windowId){
+  chrome.windows.getCurrent({populate: true}, function(currentWindow) {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+      var current = currentWindow.tabs.filter(function(tab) {
+        return tab.active;
+      })[0];
+      HandleUpdateWindowChange(current.id)
+    });
+  });
+}
+
+function HandleUpdateWindowChange(tabId){
+  initialUpdate(new Date(), tabId)
+}
+
+function initialUpdate(t, tabId){
+  activeTabId = tabId;
+  timeStart = t;
+}
+
+ function update(){
+   let now = new Date();
+   let description = FormatDuration(now - timeStart)
+   chrome.browserAction.setBadgeText({ 'tabId': activeTabId, 'text': description});
+}
+
+setInterval(update, 1000);
 
 chrome.tabs.onUpdated.addListener(HandleUpdate);
 chrome.tabs.onRemoved.addListener(HandleUpdate);
 chrome.tabs.onReplaced.addListener(HandleUpdate);
-//document.addEventListener('DOMContentLoaded', init);
+chrome.tabs.onActivated.addListener(HandleUpdateActiveTab);
+chrome.windows.onFocusChanged.addListener(WindowHasChanged)
---
+++ content.js
@@ -0,0 +1,14 @@
+var x = document.getElementsByTagName("BODY")[0]
+
+x.innerHTML += `<h1 style="
+    color: white; 
+    position: absolute;
+    z-index: 500;
+    top: 10px;
+    left: 20px;
+    padding: 3px;
+    line-height: 18px;
+    background: #00000075;
+    ">00.00<h1>`
+
+console.log('oh')
